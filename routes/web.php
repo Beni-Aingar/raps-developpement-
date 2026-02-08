a74380f9366c14386e70a9a0298af707dd92100b
BENI <beain0@gmail.com>
2026-02-08 22:59:02 +0100

    Ajout route de secours migration

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/init-db', function () { try { Artisan::call('migrate:fresh', ['--force' => true]); return "Base de données initialisée avec succès !"; } catch (\Exception $e) { return "Erreur lors de la migration : " . $e->getMessage(); } })
