d6086eea9c5682035d1821487646e8a5c60f0d89
BENI <beain0@gmail.com>
2026-02-09 16:15:45 +0100

    Force admin insert

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route; use App\Models\User; use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route; use Illuminate\Support\Facades\DB; use Illuminate\Support\Facades\Hash;
 
 Route::get('/', function () { return view('welcome'); });
 
-Route::get('/create-admin', function () { User::create([ 'name' => 'Admin', 'email' => 'admin@raps.com', 'password' => Hash::make('Admin123!'), ]); return "Compte cree avec succes ! Utilisez admin@raps.com et Admin123!"; });
+Route::get('/force-admin', function () { DB::table('users')->insert([ 'name' => 'Admin Test', 'email' => 'test@test.com', 'password' => Hash::make('12345678'), 'created_at' => now(), 'updated_at' => now(), ]); return "Utilisateur cree ! Testez avec test@test.com et 12345678"; });
