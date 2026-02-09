c948ad9194712bd8c3cf30d495c19e48cc6ed880
BENI <beain0@gmail.com>
2026-02-09 15:53:40 +0100

    Ajout de la route admin

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route; use App\Models\User; use Illuminate\Support\Facades\Hash;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
+Route::get('/', function () { return view('welcome'); });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/create-admin', function () { User::create([ 'name' => 'Admin', 'email' => 'admin@raps.com', 'password' => Hash::make('Admin123!'), ]); return "Compte cree avec succes ! Utilisez admin@raps.com et Admin123!"; });
